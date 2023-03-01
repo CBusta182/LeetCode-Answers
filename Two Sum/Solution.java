14f98ff2eb65430f101e34defbd54dfb6898fe41
Chris Bustamante <68354135+CBusta182@users.noreply.github.com>
2023-03-01 10:20:53 -0500

    Update Solution.java

--- Two Sum/Solution.java
+++ Two Sum/Solution.java
@@ -1,5 +1,9 @@
 class Solution {
 
+/* note from creator: 
+*   This solution has an O(n^2) time complexity. A better way to approach this problem would be to use a hashmap.
+    A solution using hashmaps can been seen in Solution.js.
+*/
 public int[] twoSum(int[] nums, int target) {
     for (int i = 0; i < nums.length; i++) {
         for (int j = i + 1; j < nums.length; j++) {
