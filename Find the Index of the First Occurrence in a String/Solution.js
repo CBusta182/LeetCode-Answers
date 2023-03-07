946c6412a6d717121474d05f79c88b2fca853717
Chris Bustamante <68354135+CBusta182@users.noreply.github.com>
2023-03-07 16:05:01 -0500

    Update Solution.js

--- Find the Index of the First Occurrence in a String/Solution.js
+++ Find the Index of the First Occurrence in a String/Solution.js
@@ -1,9 +1,3 @@
-/**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
- */
-
 // Solution utilizing built in JavaScript functions.
 var strStr = function( haystack, needle ) {
     if ( !haystack.includes( needle ) ) {
@@ -25,14 +19,19 @@ var strStr = function(haystack, needle) {
     let result = -1;
 
     for ( let i = 0; i <= hayLength - 1; i++ ) {
+        // check if the letter of the haystack at index i is the same as the first letter of the needle
+        // if it isn't the same, move onto the next letter.
         if ( haystack[ i ] !== needle[0] ) {
             continue; 
         }
 
+        // we are starting at i + 1 and at 1 since we already checked if the first letters are the same.
         let hayIndex = i + 1;
         let needleIndex = 1;
 
+        // while haystack and the needle still have letters, itterate through them.
         while ( haystack[ hayIndex ] && needle[ needleIndex ] ) {
+            // if letter in the haystack does not equal the letter in the needle, break out of the while loop
             if ( haystack[ hayIndex ] != needle[ needleIndex ] ){
                 break;
             }
@@ -41,6 +40,7 @@ var strStr = function(haystack, needle) {
             hayIndex++;
         }
 
+        // break out of the for loop if the needleIndex and the needleLength are the same because that means we have found a match.
         if ( needleIndex === needleLength ) {
             result = i;
 
