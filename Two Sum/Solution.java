eba79f5b67b8c43bd2f571015951cd7160bf3341
Chris Bustamante <68354135+CBusta182@users.noreply.github.com>
2023-03-07 16:17:03 -0500

    Update Solution.java

--- Two Sum/Solution.java
+++ Two Sum/Solution.java
@@ -1,8 +1,9 @@
 class Solution {
 
-/* note from creator: 
-*   This solution has an O(n^2) time complexity. A better way to approach this problem would be to use a hashmap.
-    A solution using hashmaps can been seen in Solution.js.
+/* 
+    This solution has an O(n^2) time complexity as it is a brute force approach to solving this.
+    Here I'm iterating over all the numbers twice, checking every possible combination of numbers to see if it equals the target.
+    A better way to approach this problem would be to use a hashmap which can be seen in Solution.js.
 */
 public int[] twoSum(int[] nums, int target) {
     for (int i = 0; i < nums.length; i++) {
